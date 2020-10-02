0bce1bc0966f5b60d44bddfd8a8cf4fc929e2984
flaviu2001 <craciunflaviu1@gmail.com>
2020-10-03 00:19:38 +0300

    Added Home Fragment along with data binding.

---
+++ app/src/main/java/com/example/timetable/HomeFragment.kt
@@ -0,0 +1,35 @@
+package com.example.timetable
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.timetable.databinding.HomeFragmentBinding
+
+class HomeFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = HomeFragment()
+    }
+
+    private lateinit var binding: HomeFragmentBinding
+    private lateinit var viewModel: HomeViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.home_fragment, container, false)
+        binding.lifecycleOwner = this
+        return binding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
+    }
+
+}
