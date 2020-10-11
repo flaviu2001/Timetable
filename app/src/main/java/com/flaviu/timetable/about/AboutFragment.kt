f7ebaca11101d58c73724b933a4747e8e0390cb8
flaviu2001 <craciunflaviu1@gmail.com>
2020-10-12 02:56:58 +0300

    Added about fragment

---
+++ app/src/main/java/com/flaviu/timetable/about/AboutFragment.kt
@@ -0,0 +1,22 @@
+package com.flaviu.timetable.about
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.flaviu.timetable.R
+
+class AboutFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.about_fragment, container, false)
+    }
+}
