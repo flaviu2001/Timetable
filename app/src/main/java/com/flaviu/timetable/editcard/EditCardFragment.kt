0b4c417463336384bfcd28bc13cbf2bbe94325bd
flaviu2001 <craciunflaviu1@gmail.com>
2020-10-15 02:49:22 +0300

    Android studio 4.1 compatibility

--- app/src/main/java/com/flaviu/timetable/editcard/EditCardFragment.kt
+++ app/src/main/java/com/flaviu/timetable/editcard/EditCardFragment.kt
@@ -3,7 +3,6 @@ package com.flaviu.timetable.editcard
 import android.os.Bundle
 import android.view.*
 import android.widget.Toast
-import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
